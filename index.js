4c5f32591c19ad141e626408f5b23a3a46cfbdd7
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2025-12-04 08:55:46 -0400

    Mudando valor

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
+
 app.use((req, res) => {
     res.json({
         name: 'ipad 64Gb',
-        price: 1959.90,
+        price: 1959.99,
         discount: 0.15
     })
 })
